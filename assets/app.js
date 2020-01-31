44c5d7cdac5ce993136913f9e07d6222f7e69068
lco1220 <leorieleco@gmail.com>
2020-01-31 15:30:36 +0800

    accordions added

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 const feature__link = document.querySelectorAll('.feature__link');
 const feature__items = document.querySelectorAll('.feature__item');
+const questions__btn = document.querySelectorAll('.accordion__button');
 
 feature__link.forEach(link => {
   link.addEventListener('click', e => {
@@ -12,6 +13,22 @@ feature__link.forEach(link => {
     if(e.currentTarget.id == 'easy') {
       open__feature(e.currentTarget, 'easy-sharing');
     }
+  });
+});
+
+questions__btn.forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.currentTarget.classList.toggle('accordion__button--active');
+
+    if(e.currentTarget.nextElementSibling.classList.contains('accordion__panel--active')) {
+      e.currentTarget.nextElementSibling.classList.remove('accordion__panel--active');
+    } else {
+      e.currentTarget.nextElementSibling.classList.add('accordion__panel--active');
+    }
+
+    // console.log(e.currentTarget.nextElementSibling.classList.contains('accordion__panel--active'))
+    // console.log(e.currentTarget.classList)
+    // console.log(e.currentTarget);
   });
 });
 
@@ -28,3 +45,4 @@ function open__feature(link, item) {
   document.getElementById(item).className += ' feature__item--active';
   link.className += ' feature__link--active';
 }
+
