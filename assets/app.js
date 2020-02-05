d7d0647e820bc43ee684e453aaf571549a81060a
lco1220 <leorieleco@gmail.com>
2020-02-05 18:18:33 +0800

    accordion added

--- assets/app.js
+++ assets/app.js
@@ -5,42 +5,44 @@ const form = document.getElementById('form');
 const email = document.getElementById('email');
 let input;
 
+// console.log(feature__link, feature__items);
+
 // Show input message
-function showMessage(input, alert ,message) {
-  // console.log(input.parentElement.parentElement)
-  const input__parent = input.parentElement.parentElement;
-  input__parent.className = `form__input--${alert}`;
-  const small = input__parent.querySelector('small');
-  small.textContent = message;
+// function showMessage(input, alert ,message) {
+//   // console.log(input.parentElement.parentElement)
+//   const input__parent = input.parentElement.parentElement;
+//   input__parent.className = `form__input--${alert}`;
+//   const small = input__parent.querySelector('small');
+//   small.textContent = message;
 
-  setTimeout(
-    function remove__color(){
-    input__parent.classList.remove(`form__input--${alert}`);
-    small.textContent = "";
-  }, 5000);
+//   setTimeout(
+//     function remove__color(){
+//     input__parent.classList.remove(`form__input--${alert}`);
+//     small.textContent = "";
+//   }, 5000);
 
-  setTimeout( 
-    function() {
-      input.value = "";
-    }, 10000
-  );
+//   setTimeout( 
+//     function() {
+//       input.value = "";
+//     }, 10000
+//   );
 
-}
+// }
 
 // Check email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showMessage(input,'success','Valid email. Thank you!');
-  } else {
-    showMessage(input, 'error', 'Whoops, make sure it\'s an email');
-  }
-}
+// function checkEmail(input) {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   if (re.test(input.value.trim())) {
+//     showMessage(input,'success','Valid email. Thank you!');
+//   } else {
+//     showMessage(input, 'error', 'Whoops, make sure it\'s an email');
+//   }
+// }
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  checkEmail(email);
-});
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   checkEmail(email);
+// });
 
 feature__link.forEach(link => {
   link.addEventListener('click', e => {
@@ -60,15 +62,11 @@ questions__btn.forEach(btn => {
   btn.addEventListener('click', e => {
     e.currentTarget.classList.toggle('accordion__button--active');
 
-    if(e.currentTarget.nextElementSibling.classList.contains('accordion__panel--active')) {
-      e.currentTarget.nextElementSibling.classList.remove('accordion__panel--active');
+    if(e.currentTarget.nextElementSibling.classList.contains('accordion__content--active')) {
+      e.currentTarget.nextElementSibling.classList.remove('accordion__content--active');
     } else {
-      e.currentTarget.nextElementSibling.classList.add('accordion__panel--active');
+      e.currentTarget.nextElementSibling.classList.add('accordion__content--active');
     }
-
-    // console.log(e.currentTarget.nextElementSibling.classList.contains('accordion__panel--active'))
-    // console.log(e.currentTarget.classList)
-    // console.log(e.currentTarget);
   });
 });
 
